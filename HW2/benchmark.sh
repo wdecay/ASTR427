bf76040d9a43543bc05ea1f749a160f7e10de2cb
Andrew Ivanov <andrew.ivanov@live.com>
2021-02-04 03:52:10 -0800

    some renaming

--- HW2/benchmark.sh
+++ HW2/benchmark.sh
@@ -3,7 +3,7 @@
 for h in 1.0 0.3 0.1 0.03 0.01
 do
     OUTPUT=./output/step${h}
-    ./problem1 $h > "${OUTPUT}.txt"
+    ./problem2 $h > "${OUTPUT}.txt"
     gnuplot -e "data='${OUTPUT}.txt'; out='${OUTPUT}.png'; h='${h}'" solutions.plt
     rm "${OUTPUT}.txt"
 done
