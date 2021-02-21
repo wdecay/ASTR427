719966d381055967f93131d635fc97a7b3a8cffe
Andrew Ivanov <andrew.ivanov@live.com>
2021-02-21 04:13:06 -0800

    updated convergence plots

--- HW3/gnuplot/convergence.plt
+++ HW3/gnuplot/convergence.plt
@@ -1,6 +1,8 @@
 set terminal png size 700,500
 set output out
-set title "test"
+set title sprintf("Convergence (%s)", subtitle)
+set xlabel "Iteration"
+set ylabel "Error, |x_c - x_f|" enhanced
 
 set style line 1 linecolor rgb '#0060ad' linetype 1 linewidth 2
 set style line 2 linecolor rgb '#dd181f' linetype 1 linewidth 1
@@ -23,6 +25,6 @@ set ytics add ("0" minvalue)
 # https://koutny.org/2020/10/04/logscale-gnuplot.html
 clamp(x) = (x < minvalue) ? minvalue : x;
 
-plot data1 using (clamp(abs(v1-$1))) with linespoints linestyle 1 notitle,\
-     data2 using (clamp(abs(v2-$1))) with linespoints linestyle 2 notitle
+plot data1 using (clamp(abs(v1-$1))) with linespoints linestyle 1 title "Bisection",\
+     data2 using (clamp(abs(v2-$1))) with linespoints linestyle 2 title "Newton-Raphson"
 
--- HW3/runall.sh
+++ HW3/runall.sh
@@ -14,11 +14,11 @@ gnuplot -e "e=0.9; data='./output/eccentric_anomaly_bisection.txt'; out='./outpu
 ./main 3 0.9 > ./output/kepler_0.9_bisection.txt
 ./main 4 0.9 > ./output/kepler_0.9_newton.txt
 
-gnuplot -e "data1='./output/sr_bisection.txt'; data2='./output/sr_newton.txt';out='./output/sr_conv.png'" ./gnuplot/convergence.plt
+gnuplot -e "data1='./output/sr_bisection.txt'; data2='./output/sr_newton.txt';out='./output/sr_conv.png';subtitle='square root of 2.0'" ./gnuplot/convergence.plt
 
-gnuplot -e "data1='./output/kepler_0.5_bisection.txt'; data2='./output/kepler_0.5_newton.txt';out='./output/kepler_0.5_conv.png'" ./gnuplot/convergence.plt
+gnuplot -e "data1='./output/kepler_0.5_bisection.txt'; data2='./output/kepler_0.5_newton.txt';out='./output/kepler_0.5_conv.png';subtitle='Kepler''s equation, e=0.5'" ./gnuplot/convergence.plt
 
-gnuplot -e "data1='./output/kepler_0.9_bisection.txt'; data2='./output/kepler_0.9_newton.txt';out='./output/kepler_0.9_conv.png'" ./gnuplot/convergence.plt
+gnuplot -e "data1='./output/kepler_0.9_bisection.txt'; data2='./output/kepler_0.9_newton.txt';out='./output/kepler_0.9_conv.png';subtitle='Kepler''s equation, e=0.9'" ./gnuplot/convergence.plt
 
 
 ./main 6 0.96714 > ./output/eccentric_anomaly_halley.txt
