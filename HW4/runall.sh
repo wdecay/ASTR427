069b44fa300f1506271b84f092c5c6c5c4de7b0e
Andrew Ivanov <andrew.ivanov@live.com>
2021-03-07 02:01:54 -0800

    formatting

--- HW4/runall.sh
+++ HW4/runall.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 make
 
 ./main 1 > output/rot_curve_fits.txt
 ./main 2 > output/map.txt
 
-gnuplot -e "data='./output/rot_curve_fits.txt';out='./output/rot_curve_fits.png'" ./gnuplot/rot_curve_fits.plt
+gnuplot -e "data='./output/rot_curve_fits.txt';out='./output/rot_curve_fits.png'" \
+        ./gnuplot/rot_curve_fits.plt
 
 gnuplot -e "data='./output/map.txt';out='./output/map.png'" ./gnuplot/map.plt
