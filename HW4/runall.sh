fd146b679e9ce07f39ee75ddc2d9649d1bad2f44
Andrew Ivanov <andrew.ivanov@live.com>
2021-03-08 10:47:24 -0800

    minor fixes

--- HW4/runall.sh
+++ HW4/runall.sh
@@ -5,6 +5,8 @@ make
 ./main 1 > output/rot_curve_fits.txt
 ./main 2 > output/map.txt
 
+cat output/rot_curve_fits.txt
+
 gnuplot -e "data='./output/rot_curve_fits.txt';out='./output/rot_curve_fits.png'" \
         ./gnuplot/rot_curve_fits.plt
 
